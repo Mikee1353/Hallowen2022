939afbb8210dd712b1a40a587447838759872aa5
Mikee1353 <murat_tunc07@windowslive.com>
2022-10-30 06:09:07 +0300

    a
    a

--- Assets/Scripts/CursorMove.cs
+++ Assets/Scripts/CursorMove.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class CursorMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public GameObject selectedObject;
+    Vector3 offset;
     void Update()
     {
-        
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (Input.GetMouseButtonDown(0))
+        {
+            Collider2D targetObject = Physics2D.OverlapPoint(mousePosition);
+            if (targetObject)
+            {
+                selectedObject = targetObject.transform.gameObject;
+                offset = selectedObject.transform.position - mousePosition;
+            }
+        }
+        if (selectedObject)
+        {
+            selectedObject.transform.position = mousePosition + offset;
+        }
+        if (Input.GetMouseButtonUp(0) && selectedObject)
+        {
+            selectedObject = null;
+        }
     }
 }
