54aabf38a1a1486eadefbcdfec960104a494df03
Mikee1353 <murat_tunc07@windowslive.com>
2022-10-30 07:47:56 +0300

    button
    button

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -1,12 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Button : MonoBehaviour
 {
     
     private void OnMouseDown()
     {
-        Destroy(this.gameObject);
+        transform.position = new Vector2(transform.localPosition.x, -2.534f);
+        CallLoadNextScene();
+    }
+
+    public void CallLoadNextScene()
+    {
+        Invoke("LoadNextScene", 1);
+    }
+
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
     }
 }
